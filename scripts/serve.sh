8f3f9dec1115efec3de0e35d2620e54b67237a80
Albert Borsos <albertborsos@me.com>
2015-04-11 16:49:11 +0200

    nginx location updated to yii2

--- scripts/serve.sh
+++ scripts/serve.sh
@@ -10,7 +10,7 @@ block="server {
     charset utf-8;
 
     location / {
-        try_files \$uri \$uri/ /index.php?\$query_string;
+        try_files \$uri \$uri/ /index.php?\$args;
     }
 
     location = /favicon.ico { access_log off; log_not_found off; }
